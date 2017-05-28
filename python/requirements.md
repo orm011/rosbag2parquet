d7dba34b7dd49368056d619a7da3c38f82ce3dee
Sudeep Pillai <spillai@csail.mit.edu>
2017-05-28 13:33:59 -0400

    updates to cmakelists with boost python wrapper

---
+++ python/requirements.md
@@ -0,0 +1,3 @@
+sudo apt-get install ros-indigo-ros-type-introspection
+conda install -c conda-forge parquet-cpp=1.1.0 -y
+conda install -c conda-forge arrow=0.10.0 -y
---
+++ python/rosbag2parquet_wrapper.cpp
@@ -0,0 +1,22 @@
+// Author(s): Sudeep Pillai (spillai@csail.mit.edu), Nick Rypkema (rypkema@mit.edu)
+// License: MIT
+
+#include <boost/python.hpp>
+#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
+#include <boost/python/stl_iterator.hpp>
+
+namespace py = boost::python;
+
+BOOST_PYTHON_FUNCTION_OVERLOADS(rosbag2parquet_overloads, 
+                                rosbag2parquet, 2, 4)
+
+
+BOOST_PYTHON_MODULE(rosbag2parquetpy)
+{
+  py::scope scope = py::scope();
+  
+  // py::def("rosbag2parquet", &rosbag2parquet_overloads,
+  //         py::args("bagfile", "opath"), py::arg("max_mbs")=-1, py::arg("verbose")=false);
+  py::def("rosbag2parquet", rosbag2parquet, rosbag2parquet_overloads());
+
+} 
